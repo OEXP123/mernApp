56e4aa6919d064a99ffe86b6fb49c349244edc23
OEXP123 <oexpproject4@gmail.com>
2021-10-29 16:04:23 +0530

    first commit

---
+++ server/models/userSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hashing the Password
+userSchema.pre('save', async function(next){
+  console.log("Hi from Inside Pre Save method..!!")
+if(this.isModified('password')){
+  this.password = await bcrypt.hash(this.password, 12);
+  this.cpassword = await bcrypt.hash(this.cpassword, 12);
+}
+next();
+});
+
+const User = mongoose.model("USER", userSchema);
+
+module.exports = User;
---
+++ server/router/auth.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+
+require("../db/conn");
+const User = require("../models/userSchema");
+
+router.get("/", (req, res) => {
+  res.send("Hello World!!2");
+});
+
+// Promises
+// router.post("/register", (req, res) => {
+//   const { name, email, phone, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !password || !cpassword) {
+//     return res.status(422).json({ error: "Fill all the required field" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "Email already exists" });
+//       }
+
+//       const user = new User({ name, email, phone, password, cpassword });
+
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "User registered Successfully" });
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     })
+//     .catch((err) => res.status(500).json({ message: "Failed to register" }));
+// });
+
+// Async-Await
+router.post("/register", async(req, res) => {
+  const { name, email, phone, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !password || !cpassword) {
+    return res.status(422).json({ error: "Fill all the required field" });
+  }
+try{
+  const userExist = await User.findOne({ email: email })
+  if (userExist) {
+    return res.status(422).json({ error: "Email already exists" });
+  }
+  else if(password != cpassword){
+    return res.status(422).json({ error: "Password didn't match..!!" });
+  }
+  else{
+    const user = new User({ name, email, phone, password, cpassword });
+   //yahe pe pre save function call karenge from userSchema for password
+
+      await user.save()
+         res.status(201).json({ message: "User registered Successfully" });   
+    }
+  }
+    catch(err){
+       res.status(500).json({ message: "Failed to register", err})
+    }
+  });
+
+  //login route {POST Request}
+  router.post('/signin',async(req, res) => {
+    const{email, password} = req.body;
+    try{
+      if(!email || !password){
+      return res.status(400).json({Error: "Please fill all the requird fields..!!"})
+      }
+   
+    const userLogin = await User.findOne({email:email});
+    console.log(userLogin)
+        if(!userLogin) {
+          res.status(400).json({Error:"Email Not found, Register first..!!"})
+        }
+        else{
+          res.status(200).json({message: "User Signin scucessfull..!!"})
+        }
+
+    }
+        catch(err){
+      console.log(err);
+    }
+  
+   
+  })
+
+
+
+
+
+
+
+
+
+// router.get("/about", (req, res) => {
+//   res.send("Welcome to about page");
+// });
+
+// router.get("/contact", (req, res) => {
+//   res.send("Welcome to contact page");
+// });
+
+// router.get("/login", (req, res) => {
+//   res.send("Welcome to login page");
+// });
+
+// router.get("/register", (req, res) => {
+//   res.send("Welcome to register page");
+// });
+
+module.exports = router;
